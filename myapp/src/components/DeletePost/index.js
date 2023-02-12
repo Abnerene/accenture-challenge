035677fff6ea551bc55782fb2e294593a0092e14
Abner Barrios <abnerenergy@gmail.com>
2023-02-12 13:00:54 -0600

    styles for post delete

--- myapp/src/components/DeletePost/index.js
+++ myapp/src/components/DeletePost/index.js
@@ -9,9 +9,9 @@ export default function DeletePost({postIdToDelete, setDeletePost}){
             <div className="out-pop-up">
                 <div className="pop-up">
                     <h4>Are you sure to delete this post?</h4>
-                    <div>
-                        <button onClick={()=> setDeletePost(false)}>Cancel</button>
-                        <button onClick={()=> deletePost(postIdToDelete,setDeletePost) }>Delete</button>
+                    <div className="pop-up-buttons">
+                        <button className="button" onClick={()=> setDeletePost(false)}>Cancel</button>
+                        <button className="button BGred white" onClick={()=> deletePost(postIdToDelete,setDeletePost) }>Delete</button>
                     </div>
                 </div>
             </div>
