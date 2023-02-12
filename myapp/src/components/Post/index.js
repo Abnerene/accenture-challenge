ab4f107b74abe2e38151f26927d5561b5d5ca9c4
Abner Barrios <abnerenergy@gmail.com>
2023-02-12 11:09:10 -0600

    adding edit and delete buttons in user posts

--- myapp/src/components/Post/index.js
+++ myapp/src/components/Post/index.js
@@ -3,16 +3,28 @@ import './Post.css'
 
 export default function Post({value}){
     // https://source.unsplash.com/random/?${value.category}&${value.id}
+    
+    let postActions= ""
+
+    if(value.userId===100){
+        postActions = <div className="post-actions-container">
+                        <div>🖋️</div><div>🗑️</div>
+                       </div> 
+    }
+    
     return (
-        
         <div className="post-item" Style={`background-image: url('https://source.unsplash.com/random/?${value.category}&${value.id}')`} >
             <div className="post-item-inside">
                 <h1 className="text-lg capitalize">{value.title}</h1>
                 <div>3 Coments</div>   
                 <p>{value.body}</p>
-                <div className="capitalize">{value.category}</div> 
+                <div className="tag-actions-container">
+                    <div className="capitalize category-post-tag">{value.category}</div> 
+                    {postActions}
+                </div>
             </div>
         </div>
-    )
+        )
+    
 }
 
